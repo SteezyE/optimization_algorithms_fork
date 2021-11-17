85ad2944f191ab954e6c19127ceac64536273d0b
SteezyE <eliaspichottka@gmail.com>
2021-11-17 18:30:03 +0100

    refactor for grading

--- assignments/a2_newton_plot/solution.py
+++ assignments/a2_newton_plot/solution.py
@@ -1,8 +1,73 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
 import sys
+
 sys.path.append("../../..")
 
 from optimization_algorithms.interface.nlp_solver import NLPSolver
+from optimization_algorithms.interface.mathematical_program import MathematicalProgram
+
+class Plot:
+    def plotFunc(self, f, bounds_lo, bounds_up, trace_xy = None, trace_z = None):
+        x = np.linspace(bounds_lo[0], bounds_up[0], 30)
+        y = np.linspace(bounds_lo[1], bounds_up[1], 30)
+        xMesh, yMesh = np.meshgrid(x, y, indexing='ij')
+        zMesh = np.zeros_like(xMesh)
+
+        for i in range(x.shape[0]):
+            for j in range(y.shape[0]):
+                zMesh[i,j] = f([xMesh[i,j], yMesh[i,j]])
+
+        fig = plt.figure(figsize=(12,6))
+        ax1 = fig.add_subplot(121, projection="3d")
+        surf = ax1.plot_surface(xMesh, yMesh, zMesh, cmap=cm.coolwarm)
+
+        if trace_xy is not None: ax1.plot(trace_xy[:,0], trace_xy[:,1], trace_z[:,0], 'ko-', linewidth=1.0, markersize=2.0)
+
+        fig.colorbar(surf)
+        ax1.set_xlabel('x')
+        ax1.set_ylabel('y')
+        ax1.set_zlabel('f')
+        ax2 = fig.add_subplot(122)
+        surf2 = plt.contourf(xMesh, yMesh, zMesh, cmap=cm.coolwarm)
+
+        if trace_xy is not None: ax2.plot(trace_xy[:,0], trace_xy[:,1], 'ko-', linewidth=1.0, markersize=2.0)
+
+        fig.colorbar(surf2)
+        ax2.set_xlabel('x')
+        ax2.set_ylabel('y')
+
+        plt.show()
+
+class Square(MathematicalProgram):
+    def __init__(self,C):
+        self.C = C
+
+    def evaluate(self, x):
+        y = x @ self.C @ x       
+        J = x @ self.C + x @ self.C.T 
+        return np.array([y]), J.reshape(1,-1)
+    
+    def getFHessian(self, x):
+        return 2 * self.C
+
+class Hole(MathematicalProgram):
+
+    def __init__(self, C, a):
+        self.C = C
+        self.aa = a*a
+
+    def evaluate(self, x):
+        y = (x @ self.C @ x) / (x @ self.C @ x + self.aa) 
+        J = ((x @ self.C + x @ self.C.T) * self.aa) / (x @ self.C @ x + self.aa)**2
+        return np.array([y]), J.reshape(1,-1)
+
+    def getFHessian(self, x):
+        t = x @ self.C @ x
+        vt = x @ self.C + x @ self.C.T
+        return (((2*self.aa*self.C * (t + self.aa)**2) - (vt * self.aa).T * (2 * (t + self.aa) * vt)) / (t + self.aa)**4).T
 
 class Gradient(NLPSolver):
     def __init__(self):
--- assignments/a2_newton_plot/test.py
+++ assignments/a2_newton_plot/test.py
@@ -8,14 +8,14 @@ from optimization_algorithms.interface.nlp_solver import  NLPSolver
 from optimization_algorithms.interface.mathematical_program import MathematicalProgram
 from optimization_algorithms.interface.mathematical_program_traced import  MathematicalProgramTraced
 
-from functions import Square, Hole 
-from solver import Gradient 
+from solution import Gradient, Square, Hole, Plot
 
 class testGradientDescent(unittest.TestCase):
 
     dim = 100 
     a = 0.1
     C = np.eye(dim) 
+    C2 = np.array([[10.0, 0.0],[0.0, 1.0]])
     Cinv = np.eye(dim) 
     for i in range(dim):
         C[i,i] *= 10**(i/~-dim)
@@ -23,6 +23,18 @@ class testGradientDescent(unittest.TestCase):
 
     # print(C)
     # print(Cinv)
+
+    def testSquarePlot(self):
+        square = Square(self.C2)
+        plot = Plot()
+        plot.plotFunc(lambda x: square.evaluate(x)[0][0], [-2,-2], [2,2])
+        self.assertTrue(True)
+
+    def testHolePlot(self):
+        hole = Hole(self.C2, self.a) 
+        plot = Plot()
+        plot.plotFunc(lambda x: hole.evaluate(x)[0][0], [-1,-1], [1,1])
+        self.assertTrue(True)
 
     def testSquareFunction(self): 
         print("\n---[testing square function (dim=%d) w/o C^-1]------------------------" % self.dim)
